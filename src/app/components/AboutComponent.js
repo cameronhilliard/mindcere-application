ca736b45fb2f872a84c3ac09111a6b02877309ea
Cameron Hilliard <cameronhilliard@outlook.com>
2025-03-14 13:55:51 -0300

    updated escape

--- src/app/components/AboutComponent.js
+++ src/app/components/AboutComponent.js
@@ -4,7 +4,7 @@ const AboutComponent = () => {
   return (
     <section id="about" className='flex flex-col font-outfit text-maintext py-32 p-6 items-center  md:max-w-3xl mx-auto '>
         <h1 className='font-outfit text-4xl p-6'>About</h1>
-        <p className='text-xl/10 text-center'>I created MindCere out of a deep curiosity about brain health, sparked by my own experience with a seizure. This journey isn’t just about understanding the brain—it's about exploring ways to strengthen and protect it. Through MindCere, I aim to uncover insights that could help not only me but others who may be looking for ways to support cognitive well-being and possibly prevent provoked seizures.</p>
+        <p className='text-xl/10 text-center'>I created MindCere out of a deep curiosity about brain health, sparked by my own experience with a seizure. This journey isn&rsquo;t just about understanding the brain&mdash;it&apos;s about exploring ways to strengthen and protect it. Through MindCere, I aim to uncover insights that could help not only me but others who may be looking for ways to support cognitive well-being and possibly prevent provoked seizures</p>
     </section>
   )
 }
